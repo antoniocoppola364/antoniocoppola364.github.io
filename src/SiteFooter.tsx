c5e638547f02679545ffa38570af7466243077d8
antonio.coppola <antonio.coppola@tum.de>
2024-05-14 14:34:44 +0200

    modifications

--- src/SiteFooter.tsx
+++ src/SiteFooter.tsx
@@ -10,7 +10,7 @@ const pages = ['Impressum']
 const external = [['TUM', 'https://www.tum.de']]
 const affiliates = [['BMBF', 'https://www.bmbf.de/SiteGlobals/Frontend/Images/icons/_common/publisherlogo-en.svg?__blob=normal&v=5', 'https://www.bmbf.de/'], ['TUM', './data/logos/tum_logo.png', 'https://www.tum.de'],
 ['MCube', './data/logos/mcube_logo.png', 'https://mcube-cluster.de/']]
-const disclaimer = "[is it correct?] This project was funded by the German Federal Ministry of Education and Research (BMBF) under Grant 03ZU1105FA"
+const disclaimer = "This project was funded by the German Federal Ministry of Education and Research (BMBF) under Grant 03ZU1105FA"
 
 
 function FooterPages() {
